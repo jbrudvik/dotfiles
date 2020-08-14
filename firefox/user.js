205cb93b3a87f47e475b2bcf8d8389452cb42962
Jeremy Brudvik <jeremy@brudvik.me>
2020-08-13 19:34:23 -0700

    Update Firefox settings: No bksp back, Fix tabs

--- firefox/user.js
+++ firefox/user.js
@@ -1,3 +1,4 @@
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("devtools.theme", "dark");
-user_pref("browser.tabs.insertRelatedAfterCurrent", false);
+user_pref("browser.tabs.insertRelatedAfterCurrent", true);
+user_pref("browser.backspace_action", 2);
