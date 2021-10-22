c5c098b31cbcaf850eab1d7e806ea61bcc2ea20d
Jeremy Brudvik <jeremy@brudvik.me>
2021-10-22 03:50:44 -0400

    Remove Firefox dotfiles config

--- firefox/user.js
+++
@@ -1,4 +0,0 @@
-user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
-user_pref("devtools.theme", "dark");
-user_pref("browser.tabs.insertRelatedAfterCurrent", true);
-user_pref("browser.backspace_action", 2);
